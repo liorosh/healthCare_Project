0c7c0be29047226d09fc47003e0f989f3d26142b
Mirish <mirishlaen@hotmail.com>
2017-01-14 11:27:08 +0200

    Some gui related files

---
+++ src/clientInsured/Main.java
@@ -0,0 +1,39 @@
+package clientInsured;
+	
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			 // constructing our scene
+			 URL url = getClass().getResource("MakeAppoint.fxml");
+			 AnchorPane pane = FXMLLoader.load( url );
+			 Scene scene = new Scene( pane );
+
+			 // setting the stage
+			 primaryStage.setScene( scene );
+			 primaryStage.setTitle( "Hello World Demo" );
+			 primaryStage.show();
+			//BorderPane root = new BorderPane();
+			//Scene scene = new Scene(root,400,400);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//primaryStage.setScene(scene);
+			//primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
